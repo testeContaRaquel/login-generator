6d84aaac01d40f208d99a7e318a6aaedc256f291
testeContaRaquel <raquel.oliveira@irit.fr>
2020-11-24 11:18:26 +0100

    modified random pass

--- src/main/java/geco/PasswordGeneration.java
+++ src/main/java/geco/PasswordGeneration.java
@@ -3,6 +3,6 @@ package geco;
 public class PasswordGeneration {
 
     public String getRandomPassword(){
-        return "This is a random password";
+        return "This is another random password";
     }
 }
